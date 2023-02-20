b8f5c278f8473b0b7b665d95928ea08e6030bebd
eBaBy-C <udbrkve@gmail.com>
2023-02-20 10:10:03 +0800

    some array

---
+++ demo/array-practice.js
@@ -0,0 +1,26 @@
+// 查找数组对象中 age 大于 18 对象
+const filterAdult = (res) => {
+  return res.filter(i => i.age > 18)
+}
+
+let demo1 = filterAdult([
+  {age: 19, name:'Jack'},
+  {age: 5, name:'Apple'},
+  {age: 12, name:'Lynn'},
+  {age: 25, name:'David'}
+]);
+
+// 判断数组中是否所有数字都大于0
+const isAllNumPosive = (res) => {
+  
+}
+
+// 改变传入的数组 将数组的第n个放到数组开头
+const putFirst = (arr, n) => {
+}
+let arr = [1,2,3,4]
+console.log(
+putFirst([1,2,3],1)
+
+);
+
