33acfcfcee5fe684007d3bfd8792a7c4d232988f
eBaBy-C <udbrkve@gmail.com>
2023-02-26 13:16:55 +0800

    Switching options

--- demo/JavaScriptchallenge.js
+++ demo/JavaScriptchallenge.js
@@ -102,8 +102,9 @@ const seriesResistance = (arr) => {
 
 const chunk = (arr, size) =>
   arr.reduce(
-    (acc, e, i) =>
-      (i % size ? acc[acc.length - 1].push(e) : acc.push([e]), acc),
-    []
+    (acc, e, i) => {
+      return i % size ? acc[acc.length - 1].push(e) : acc.push([e]), acc
+    }, []
   );
+
 
