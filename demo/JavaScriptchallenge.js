699aa13c408d771621ffc1e0653fe9ffd3cead50
eBaBy-C <udbrkve@gmail.com>
2023-02-26 17:14:11 +0800

    fisher–yates

--- demo/JavaScriptchallenge.js
+++ demo/JavaScriptchallenge.js
@@ -107,4 +107,12 @@ const chunk = (arr, size) =>
     }, []
   );
 
+const shuffle = arr => {
+  let m = arr.length;
+  while (m > 1) {
+    let index = Math.floor(Math.random() * m--);
+    [arr[m], arr[index]] = [arr[index], arr[m]]
+  }
+  return arr
+}
 
